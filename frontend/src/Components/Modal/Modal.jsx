e59c0e4382b51ebe50f745faf9221c63116aaf6b
satyam gupta <stymngrya@gmail.com>
2023-04-03 00:40:43 +0530

    modal for create bug

---
+++ frontend/src/Components/Modal/Modal.jsx
@@ -0,0 +1,65 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    FormControl,
+    FormLabel,
+    Input,
+    Button,
+    useDisclosure,
+  } from '@chakra-ui/react'
+  import React from "react";
+  import { FcAddDatabase } from "react-icons/fc";
+
+
+function InitialFocus() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    const initialRef = React.useRef(null)
+    const finalRef = React.useRef(null)
+  
+    return (
+      <>
+        <button onClick={onOpen}>
+            <FcAddDatabase />
+            
+            </button>
+  
+        <Modal
+          initialFocusRef={initialRef}
+          finalFocusRef={finalRef}
+          isOpen={isOpen}
+          onClose={onClose}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Report Bug</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>Name</FormLabel>
+                <Input ref={initialRef} placeholder='Write Bug name' />
+              </FormControl>
+  
+             
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3}>
+                Save
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
+
+
+
+  export default InitialFocus
--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import "../Styles/dashboard.css";
+import { FcAddDatabase } from "react-icons/fc";
+import InitialFocus from "../Components/Modal/Modal";
 
 const Dashboard = () => {
+  const handleAddTasks = () => {
+    console.log("hat");
+  };
+
   return (
     <div className="dashboard_container">
       <div className="tasks_parent">
         <div className="critical_tasks tasks_container">
           <div id="critical" className="task_heading">
             <h1>Critical severity</h1>
+            <InitialFocus />
           </div>
-         
         </div>
         <div className="major_tasks tasks_container">
           <div id="major" className="task_heading ">
-            <h1>Critical severity</h1>
+            <h1>Major severity</h1>
+            <InitialFocus />
           </div>
         </div>
         <div className="medium_tasks tasks_container">
           <div id="medium" className="task_heading">
-            <h1>Critical severity</h1>
+            <h1>Medium severity</h1>
+            <InitialFocus />
           </div>
         </div>
         <div className="low_tasks tasks_container">
           <div id="low" className="task_heading">
-            <h1>Critical severity</h1>
+            <h1>Low severity</h1>
+            <InitialFocus />
           </div>
         </div>
       </div>
